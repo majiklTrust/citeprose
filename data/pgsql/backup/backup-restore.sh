259722a17e999b7a06a35c85a85220349a89b36a
Brandon R. India <brandonindia-mcs>
2026-05-07 16:03:26 +0000

    restore optional

--- data/pgsql/backup/backup-restore.sh
+++ data/pgsql/backup/backup-restore.sh
@@ -5,12 +5,11 @@
 read -p "origin database ($PGDATABASE) " ORIGIN
 echo
 if [ -z "$ORIGIN" ];then ORIGIN=$PGDATABASE;fi
+pg_dump -U $PGUSER -d $PGDATABASE -Fc -f ./backup_${ORIGIN}_$(date +%Y%m%d).dump
 
 read -p "destination database: " DESTINATION
 echo
 if [ -z "$DESTINATION" ];then echo no distination && exit 1;fi
-
-pg_dump -U $PGUSER -d $PGDATABASE -Fc -f ./backup_${ORIGIN}_$(date +%Y%m%d).dump
 
 # Restore to a new name
 createdb -U $PGUSER $DESTINATION

a0189734c6fafa550b3106f5d6a6426047bd3867
Brandon R. India <brandonindia-mcs>
2026-05-07 16:01:09 +0000

    backup restore script

---
+++ data/pgsql/backup/backup-restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+(
+
+# Backup
+read -p "origin database ($PGDATABASE) " ORIGIN
+echo
+if [ -z "$ORIGIN" ];then ORIGIN=$PGDATABASE;fi
+
+read -p "destination database: " DESTINATION
+echo
+if [ -z "$DESTINATION" ];then echo no distination && exit 1;fi
+
+pg_dump -U $PGUSER -d $PGDATABASE -Fc -f ./backup_${ORIGIN}_$(date +%Y%m%d).dump
+
+# Restore to a new name
+createdb -U $PGUSER $DESTINATION
+pg_restore -U $PGUSER -d $DESTINATION ./backup_${ORIGIN}_$(date +%Y%m%d).dump
+)

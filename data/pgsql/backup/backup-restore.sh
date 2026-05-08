0e18d2c3c056dbf1713422e8841537115119d9b0
Brandon R. India <brandonindia-mcs>
2026-05-08 15:02:26 +0000

    better messaging

--- data/pgsql/backup/backup-restore.sh
+++ data/pgsql/backup/backup-restore.sh
@@ -5,11 +5,12 @@
 read -p "origin database ($PGDATABASE) " ORIGIN
 echo
 if [ -z "$ORIGIN" ];then ORIGIN=$PGDATABASE;fi
-pg_dump -U $PGUSER -d $ORIGIN -Fc -f ./backup_${ORIGIN}_$(date +%Y%m%d).dump
+pg_dump -U $PGUSER -d $ORIGIN -Fc -f ./backup_${ORIGIN}_$(date +%Y%m%d).dump\
+  && echo backup file at: && ls -l ./backup_${ORIGIN}_$(date +%Y%m%d).dump
 
-read -p "destination database: " DESTINATION
+read -p "destination database (or empty to exit): " DESTINATION
 echo
-if [ -z "$DESTINATION" ];then echo no distination && exit 1;fi
+if [ -z "$DESTINATION" ];then echo no destination, exiting && exit 1;fi
 
 # Restore to a new name
 createdb -U $PGUSER $DESTINATION

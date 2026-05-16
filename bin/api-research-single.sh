88a83eb0037cda6aa1ed0adc8f0bf2e4510f4a9d
Brandon R. India <brandonindia-mcs>
2026-05-16 15:09:34 +0000

    api-research-single utility

---
+++ bin/api-research-single.sh
@@ -0,0 +1,5 @@
+(
+  feed_id=${1:-1}
+  echo curl -s -X POST http://localhost:3001/api/research/single   -H "Content-Type: application/json" -d '{"feedId": "'$feed_id'"}'
+  curl -s -X POST http://localhost:3001/api/research/single   -H "Content-Type: application/json" -d '{"feedId": "'$feed_id'"}'|jq
+)

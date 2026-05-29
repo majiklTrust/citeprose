9a92e3872833724a77a2c2bc1437a3e1b2e11669
Brandon R. India <brandonindia-mcs>
2026-05-29 19:17:39 +0000

    do_backup take arg1

--- data/pgsql/do_backup.sh
+++ data/pgsql/do_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (
-PGBACKUP=alpha_${PGDATABASE}_$(date +%Y%m%d_T_%H%M).dump\
+PGBACKUP=${1:-alpha}_${PGDATABASE}_$(date +%Y%m%d_T_%H%M).dump\
   && pg_dump -U $PGUSER -d $PGDATABASE -Fc -f $(pwd)/$PGBACKUP\
   && chmod 400 $(pwd)/$PGBACKUP\
   && echo $PGBACKUP

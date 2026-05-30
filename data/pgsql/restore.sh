45b781d7168346f97bff902c22100ab6621cb245
Brandon R. India <brandonindia-mcs>
2026-05-30 23:29:07 +0000

    out of sequence restore database with fixing sequence numbers

--- data/pgsql/restore.sh
+++ data/pgsql/restore.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 (
 
-    function fix_seq {
-      for t in activity_log articles_v2 feed_articles feed_topics feeds_v2 posts topics ;do
-      psql -P pager=off -t -c "
-      SELECT t.table_name,
-       t.seq_name,
-       (SELECT last_value FROM pg_sequences WHERE schemaname = 'public' AND sequencename = t.seq_name) AS seq_val,
-       t.max_id
-        FROM (
-        SELECT '$t' AS table_name, '${t}_id_seq' AS seq_name, (SELECT MAX(id) FROM $t) AS max_id
-        ) t;"
-      done
-    }
+function get_sequence_tables {
+psql -P pager=off -c "
+SELECT table_name, column_name,
+       pg_get_serial_sequence(table_name, column_name) AS sequence_name
+FROM information_schema.columns
+WHERE table_schema = 'public'
+  AND identity_generation IS NOT NULL
+   OR column_default LIKE 'nextval%'
+ORDER BY table_name;
+"
+}
+function fix_seq {
+# If seq_val >= max_id, you're good.
+
+for t in activity_log articles_v2 feed_articles feed_topics feeds_v2 posts topics ;do
+SQL="SELECT '$t' tbl, setval(
+  pg_get_serial_sequence('$t', 'id'),
+  COALESCE((SELECT MAX(id) FROM $t), 0) + 1,
+  false
+)"
+psql -P pager=off -c "$SQL"
+done
+}
+
 # Backup
 read -p "backup origin database ($PGDATABASE): " ORIGIN
 echo
@@ -27,9 +39,9 @@ if [ -z "$RESTORE" ];then echo no restore, exiting && exit 1;fi
 RESTORE=/datavol/pgbackup/$RESTORE
 if [ -r "$RESTORE" ];then :;else echo cannot read $RESTORE, exiting && exit 1;fi
 
-read -p "destination database (or empty to exit): " DESTINATION
+read -p "destination database ($ORIGIN): " DESTINATION
 echo
-if [ -z "$DESTINATION" ];then echo no destination, exiting && exit 1;fi
+if [ -z "$DESTINATION" ];then DESTINATION=$ORIGIN;fi
 
 # Restore to a new name
 if psql -Atc "SELECT 1 FROM pg_database WHERE datname='$PGDATABASE'" postgres >/dev/null; then
@@ -37,5 +49,6 @@ dropdb -i --force $DESTINATION
 fi
 createdb -U $PGUSER $DESTINATION
 pg_restore -U $PGUSER -d $DESTINATION $RESTORE
+get_sequence_tables
 fix_seq
 )

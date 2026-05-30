f8031acef2256530da992c28258fb13a42f1cf33
Brandon R. India <brandonindia-mcs>
2026-05-30 11:56:05 +0000

    removed sql from dev project

--- data/pgsql/schema/production-sql/add-member.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO memberships (auth_provider, auth_sub, tenant_id, role)
-VALUES ('auth0', 'auth0|test_user_001', '53f2e104-4192-439b-abdc-70954bfa9583', 'owner')
-ON CONFLICT DO NOTHING;
-
-INSERT INTO memberships (auth_provider, auth_sub, tenant_id, role)
-VALUES ('auth0', '***REMOVED***', '53f2e104-4192-439b-abdc-70954bfa9583', 'owner')
-ON CONFLICT DO NOTHING;

fee4f00338ac782a3cdb6aca4f8c3c2f556473f1
Brandon R. India <brandonindia-mcs>
2026-05-31 18:51:47 +0000

    extractd utility

---
+++ bin/extractd.sh
@@ -0,0 +1,12 @@
+
+
+noyes() { read -p "$*? (y/N): " && if [[ ${REPLY,,} = y ]] || [[ ${REPLY,,} = yes ]]; then return 0; fi; return 1; }
+
+v="${1}"
+zip="/datavol/LinkedIn_Agent_drop/drop/LinkedIn_Agent-$v.zip"
+# if noyes "extract $zip";then 
+mkdir -p /datavol/$npm_package_name/drop/$v\
+  && unzip -o "$zip" -d /datavol/$npm_package_name/drop/$v
+# fi
+
+# npm version $v && npm run build

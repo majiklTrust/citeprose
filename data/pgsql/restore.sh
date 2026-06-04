19b3a8a7e6e02a2dcd295eb13a239358f37c59d5
Brandon R. India <brandonindia-mcs>
2026-06-04 18:56:26 +0000

    streamlined fixing sequences on restore

--- data/pgsql/restore.sh
+++ data/pgsql/restore.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 (
+function get_sequence_names {
+psql -P pager=off -t -c "
+SELECT table_name
+FROM information_schema.columns
+WHERE table_schema = 'public'
+  AND identity_generation IS NOT NULL
+   OR column_default LIKE 'nextval%'
+ORDER BY table_name;
+"
+}
 
 function get_sequence_tables {
 psql -P pager=off -c "
@@ -14,8 +24,8 @@ ORDER BY table_name;
 }
 function fix_seq {
 # If seq_val >= max_id, you're good.
-
-for t in activity_log articles_v2 feed_articles feed_topics feeds_v2 posts topics ;do
+table_list=($(get_sequence_names))
+for t in ${table_list[@]};do
 SQL="SELECT '$t' tbl, setval(
   pg_get_serial_sequence('$t', 'id'),
   COALESCE((SELECT MAX(id) FROM $t), 0) + 1,
@@ -23,6 +33,14 @@ SQL="SELECT '$t' tbl, setval(
 )"
 psql -P pager=off -c "$SQL"
 done
+# for t in activity_log articles_v2 feed_articles feed_topics feeds_v2 posts topics ;do
+# SQL="SELECT '$t' tbl, setval(
+#   pg_get_serial_sequence('$t', 'id'),
+#   COALESCE((SELECT MAX(id) FROM $t), 0) + 1,
+#   false
+# )"
+# psql -P pager=off -c "$SQL"
+# done
 }
 
 # Backup
@@ -49,6 +67,6 @@ dropdb -i --force $DESTINATION
 fi
 createdb -U $PGUSER $DESTINATION
 pg_restore -U $PGUSER -d $DESTINATION $RESTORE
-get_sequence_tables
+# get_sequence_tables
 fix_seq
 )
